fd647d352a70e94d5d2325e9b8921c560e2e1c11
SarahAlfakeeh <sarahalfakeeh@gmail.com>
2015-11-28 11:10:42 +0300

    initial commit

---
+++ ex3.php
@@ -0,0 +1,16 @@
+<?php
+print "<h2>PHP is Fun!</h2>";
+print "Hello world!<br>";
+print "I'm about to learn PHP!";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title></title>
+    </head>
+    <body>
+        
+    </body>
+</html>
---
+++ ex4.php
@@ -0,0 +1,15 @@
+<?php
+$x = 5985;
+var_dump($x);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <title></title>
+    </head>
+    <body>
+        
+    </body>
+</html>
